7125adb5e594de221119f0a589ed1c9bab07cc59
Phil Jirsa <phil.jirsa@microsoft.com>
2024-07-08 16:56:37 -0500

    add ai-studio-sample-app

---
+++ src/sample-app-aoai-chatGPT/start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export NODE_OPTIONS=--max_old_space_size=8192
+
+echo ""
+echo "Restoring frontend npm packages"
+echo ""
+cd frontend
+npm install
+if [ $? -ne 0 ]; then
+    echo "Failed to restore frontend npm packages"
+    exit $?
+fi
+
+echo ""
+echo "Building frontend"
+echo ""
+npm run build
+if [ $? -ne 0 ]; then
+    echo "Failed to build frontend"
+    exit $?
+fi
+
+cd ..
+. ./scripts/loadenv.sh
+
+echo ""
+echo "Starting backend"
+echo ""
+./.venv/bin/python -m quart run --port=50505 --host=127.0.0.1 --reload
+if [ $? -ne 0 ]; then
+    echo "Failed to start backend"
+    exit $?
+fi

d9e298a460de52433f053b31bdb4d54f8d048544
MJ Schanne <mjschanne@microsoft.com>
2024-07-11 16:14:32 -0400

    Resolve duplicate SearchResult.cs

--- src/Api/Models/SearchResult.cs
+++ src/Api/Models/SearchResult.cs
@@ -2,19 +2,19 @@
 
 public class QueryResult
 {
-    public string IndexRecordId { get; set; }
-    public int NoteId { get; set; }
-    public string NoteChunk { get; set; }
-    public string NoteChunkOrder { get; set; }
-    public int CSN { get; set; }
-    public int MRN { get; set; }
-    public string NoteType { get; set; }
-    public string NoteStatus { get; set; }
-    public string AuthorId { get; set; }
-    public string AuthorFirstName { get; set; }
-    public string AuthorLastName { get; set; }
-    public string Department { get; set; }
-    public string Gender { get; set; }
-    public DateTime BirthDate { get; set; }
+    public string IndexRecordId { get; set; } = string.Empty;
+    public int NoteId { get; set; } = 0;
+    public string NoteChunk { get; set; } = string.Empty;
+    public string NoteChunkOrder { get; set; } = string.Empty;
+    public int CSN { get; set; } = 0;
+    public int MRN { get; set; } = 0;
+    public string NoteType { get; set; } = string.Empty;
+    public string NoteStatus { get; set; } = string.Empty;
+    public string AuthorId { get; set; } = string.Empty;
+    public string AuthorFirstName { get; set; } = string.Empty;
+    public string AuthorLastName { get; set; } = string.Empty;
+    public string Department { get; set; } = string.Empty;
+    public string Gender { get; set; } = string.Empty;
+    public DateTime BirthDate { get; set; } = DateTime.UtcNow;
 }
 

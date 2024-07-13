d33d1e46f573ebc3c4f3db3c5dad1bc47a8e8301
MJ Schanne <mjschanne@microsoft.com>
2024-07-13 01:34:29 -0400

    Simplify FrontendSettings endpoint and resolve compiler warnings

--- src/ChatApp/ChatApp.Server/Endpoints.Api.cs
+++ src/ChatApp/ChatApp.Server/Endpoints.Api.cs
@@ -5,30 +5,10 @@ public static partial class Endpoints
 {
     public static WebApplication MapApiEndpoints(this WebApplication app)
     {
-        app.MapGet("/frontend_settings", GetFrontEndSettings)
+        app.MapGet("/frontend_settings", (HttpContext httpContext) => Results.Json(new FrontendSettings()))
             .WithName("GetFrontendSettings")
             .WithOpenApi();
 
         return app;
     }
-
-    private static IResult GetFrontEndSettings(HttpContext httpContext)
-    {
-        var settings = new FrontendSettings
-        {
-            AuthEnabled = false,
-            FeedbackEnabled = false,
-            Ui = new UiSettings
-            {
-                Title = "Damu",
-                ChatTitle = "Start chatting",
-                ChatDescription = "This chatbot is configured to answer your questions",
-                ShowShareButton = true
-            },
-            SanitizeAnswer = false
-        };
-
-        return Results.Json(settings);
-    }
-
 }

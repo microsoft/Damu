2a9aa76598b1642a7cb19adef434f39d8a04f95b
MJ Schanne <mjschanne@microsoft.com>
2024-07-15 08:30:48 -0400

    Add Conversation model

---
+++ src/ChatApp/ChatApp.Server/Models/Conversation.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ChatApp.Server.Models;
+
+// todo: change type from string to enum?
+public class Conversation
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = "conversation"; // other value types?
+    [JsonPropertyName("createdAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    [JsonPropertyName("updatedAt")]
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    [JsonPropertyName("userId")]
+    public string UserId { get; set; } = string.Empty;
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = string.Empty;
+    [JsonPropertyName("messages")]
+    public List<Message> Messages { get; set; } = new List<Message>();
+}
